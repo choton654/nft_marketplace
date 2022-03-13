737eba28ef960c475f37aea4f742a85185a301b7
Subhadeep Paul <choton654@gmail.com>
2022-03-13 22:12:15 +0530

    add purchaseitem function

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -6,8 +6,8 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Marketplace is ReentrancyGuard {
     //  state variables
-    address payable public immutable feeAccount;
-    uint256 public immutable feeParcent;
+    address payable public immutable feeAccount; // the account that receives fees
+    uint256 public immutable feeParcent; // the fee percentage on sales
     uint256 public itemCount;
 
     struct Item {
@@ -27,6 +27,15 @@ contract Marketplace is ReentrancyGuard {
         uint256 tokenId,
         uint256 price,
         address indexed seller
+    );
+
+    event Bought(
+        uint256 itemId,
+        address indexed nft,
+        uint256 tokenId,
+        uint256 price,
+        address indexed seller,
+        address indexed buyer
     );
 
     constructor(uint256 _feeParcent) {
@@ -55,5 +64,41 @@ contract Marketplace is ReentrancyGuard {
         );
 
         emit Offered(itemCount, address(_nft), _tokenId, _price, msg.sender);
+    }
+
+    function purchaseItem(uint256 _itemId) external payable nonReentrant {
+        uint256 _totalPrice = getTotalPrice(_itemId);
+        Item storage item = items[_itemId];
+
+        require(_itemId > 0 && _itemId <= itemCount, "item does not exist");
+        require(
+            msg.sender >= _totalPrice,
+            "not enough ether to cover item price and market fee"
+        );
+        require(!item.sold, "item already sold");
+
+        // pay seller and feeaccount
+        item.seller.transfer(item.price);
+        feeAccount.transfer(_totalPrice - item.price);
+
+        // update item to sold
+        item.sold = true;
+
+        // transfer nft to buyer
+        item.nft.transferFrom(address(this), msg.sender, item.tokenId);
+
+        // emit bought event
+        emit Bought(
+            _itemId,
+            address(item.nft),
+            item.tokenId,
+            item.price,
+            item.seller,
+            msg.sender
+        );
+    }
+
+    function getTotalPrice(uint256 _itemId) public view returns (uint256) {
+        return ((items[_itemId].price * (100 + feeParcent)) / 100);
     }
 }
